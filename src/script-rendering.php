f5a2977afc5d225448301df287edfdd0ec9d664b
“Tuomas <tume91@gmail.com>
2025-06-17 12:03:08 +0300

    Change consent mode and cookiebot script order. Version pump.

--- src/script-rendering.php
+++ src/script-rendering.php
@@ -18,13 +18,6 @@ function mtps_head_scripts() {
     $cookiebot_id          = apply_filters( 'mtps_cookiebot_id', $cookiebot_field_value );
 
     if ( $cookiebot_id && $cookiebot_no_head !== '1' ) {
-        $current_locale = substr( get_locale(), 0, 2 );
-        $cookiebot_code = '<script async id="Cookiebot" data-culture="' . esc_attr( $current_locale ) . '" src="https://consent.cookiebot.com/uc.js" data-cbid="' . esc_attr( $cookiebot_id ) . '" data-blockingmode="auto"></script>';
-        $cookiebot_code = apply_filters( 'mtps_cookiebot_code', $cookiebot_code, $cookiebot_id );
-        // phpcs:disable
-        echo $cookiebot_code;
-        // phpcs:enable
-
         // If we have Cookiebot in use, we need to set the consent mode.
         $cookiebot_consent_mode = "
             <!-- Google Consent Mode -->
@@ -51,6 +44,13 @@ function mtps_head_scripts() {
         $cookiebot_consent_mode = apply_filters( 'mtps_cookiebot_consent_mode', $cookiebot_consent_mode );
         // phpcs:disable
         echo $cookiebot_consent_mode;
+        // phpcs:enable
+
+        $current_locale = substr( get_locale(), 0, 2 );
+        $cookiebot_code = '<script async id="Cookiebot" data-culture="' . esc_attr( $current_locale ) . '" src="https://consent.cookiebot.com/uc.js" data-cbid="' . esc_attr( $cookiebot_id ) . '" data-blockingmode="auto"></script>';
+        $cookiebot_code = apply_filters( 'mtps_cookiebot_code', $cookiebot_code, $cookiebot_id );
+        // phpcs:disable
+        echo $cookiebot_code;
         // phpcs:enable
     }
 
